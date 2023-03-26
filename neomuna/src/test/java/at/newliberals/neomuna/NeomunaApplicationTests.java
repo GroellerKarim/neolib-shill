c0b682440d6aa4b0ee23ee55078d000902d41831
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-03-26 20:47:09 +0200

    Initial Commit and Project Setup

---
+++ neomuna/src/test/java/at/newliberals/neomuna/NeomunaApplicationTests.java
@@ -0,0 +1,13 @@
+package at.newliberals.neomuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeomunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ vienna/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navbar">
+    <p>Home</p>
+    <p>Our Vision</p>
+    <p>Test</p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  display: inline-flex;
+  padding-right: 10px;
+}
+
+.logo {
+
+}
+
+.navbar {
+  background: #159957; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #159957, #155799); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #159957, #155799); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+
+  width: 100%;
+  height: 10%;
+}
+
+</style>
