cb9ebf49b877780e86383bde565bc89d8a22b545
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-03-27 00:58:31 +0200

    Working on NavbarButton Animation

--- vienna/src/components/Navbar.vue
+++ vienna/src/components/Navbar.vue
@@ -4,17 +4,15 @@
 
 <template>
   <div class="navbar">
-    <p>Home</p>
-    <p>Our Vision</p>
-    <p>Test</p>
+    <div class="buttongroup">
+      <button class="navigationButton">Home</button>
+      <button class="navigationButton">Our Vision</button>
+      <button class="navigationButton">Test</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
-p {
-  display: inline-flex;
-  padding-right: 10px;
-}
 
 .logo {
 
@@ -31,12 +29,50 @@ p {
   justify-content: center;
 
   position: absolute;
-  left: 0px;
-  right: 0px;
-  top: 0px;
+  left: 0;
+  right: 0;
+  top: 0;
 
   width: 100%;
   height: 10%;
 }
+
+.navigationButton {
+  transition-duration: 0.2s;
+  margin: 12px;
+  font-size: 16px;
+
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 6px;
+  padding-right: 6px;
+
+  position: relative;
+
+  border: none;
+  display: inline-block;
+  color: white;
+  text-decoration: none;
+  background: rgba(255, 122, 89, 0%);
+
+}
+.navigationButton:hover::after {
+  // box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+
+  transform: scale(1);
+}
+
+.navigationButton::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  transform: scale(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: white;
+  transition: transform 0.25s ease-out;
+}
+
 
 </style>
